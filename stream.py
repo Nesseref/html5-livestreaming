9d8133555d2dce11550a8cbfd4543a98c9426184
Nesseref <orc@frogbox.es>
2016-02-13 06:35:57 +0000

    added online stream checking

--- stream.py
+++ stream.py
@@ -2,8 +2,11 @@ from flask import Flask, request, render_template, send_from_directory
 from flask.ext.api import status
 import psycopg2
 import config
+import requests
 
 app = Flask(__name__)
+
+hostname = 'http://162.243.140.45/hls/'
 
 @app.route('/auth')
 def auth():
@@ -34,7 +37,11 @@ def serve_player(username):
     if len(cur.fetchall()) == 0:
         return "No such user!"
     else:
-        return render_template('player.html', username=username);
+        r = requests.get(hostname + username + ".m3u8")
+        if r.status_code == requests.codes.ok:
+            return render_template('player.html', username=username);
+        else:
+            return "Stream offline!"
 
 if __name__== '__main__':
     app.run(host='0.0.0.0')
